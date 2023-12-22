e25f9bb3b539df3182b4d7e4ee87cb94c6c3c371
pcuestamorer <pablocuestam@outlook.com>
2023-12-22 17:57:20 +0100

    init project

---
+++ CuestasBank/src/main/java/pcuesta/cuestasbank/CuestasBankApplication.java
@@ -0,0 +1,13 @@
+package pcuesta.cuestasbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuestasBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuestasBankApplication.class, args);
+    }
+
+}
